46e0b8823e38ad281de0730ba00017e9c1f561af
Yelison <46820524+Yelison@users.noreply.github.com>
2020-10-14 21:21:18 -0400

    Add templestring test (#3)
    Co-authored-by: Yelisson <yrodriguez@gmail.com>

---
+++ src/basesJS/02-template-string.js
@@ -0,0 +1,18 @@
+
+
+
+const nombre   = 'Yelisson';
+const apellido = 'Ortiz';
+
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${ nombre } ${ apellido }`;
+
+// console.log( nombreCompleto );
+
+
+export function getSaludo(nombre = 'Ortiz') {
+    return 'Hola ' + nombre;
+}
+
+// console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
