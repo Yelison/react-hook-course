80798dcee1c6ff718fd737b444e03c8906b8a614
Yelison <46820524+Yelison@users.noreply.github.com>
2020-10-14 21:02:39 -0400

    Bases java script (#2)
    * const and let
    
    * Template string
    
    * Literal object
    
    * Array
    
    * Funcionts
    
    * Destructuring Object
    
    * Destructuring Array
    
    * Import and export
    
    * Promises
    
    * Fetch
    
    * Async await
    
    * Ternary
    
    Co-authored-by: Yelisson <yrodriguez@gmail.com>

---
+++ src/basesJS.js/01-const-let.js
@@ -0,0 +1,19 @@
+
+// Variables y Constantes
+
+const nombre = 'Yelisson';
+const apellido = 'Ortiz';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado )
+
+// var No se debe de usar...
+if ( true ) {
+    const nombre = 'Cesarina';
+    console.log(nombre)
+}
+
+console.log( valorDado );
+
